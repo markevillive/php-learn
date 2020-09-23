301ae9f924e0d64948bf6670e2408c8dd8521c10
markevillive <mark_evillive@hotmail.com>
2020-09-23 20:40:33 +0700

    start

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+     <meta charset="UTF-8">
+     <title>Learn php</title>
+    </head>
+    <body>
+    <h1>Learn PHP.</h1>
+    <h2>Пример встраивания Php в html</h2>
+
+    </body>
+</html>
